32d24f73659dbc4066df4dd4abfc133d3dec8d86
Ynoz <floriansonnleitner14@gmail.com>
2025-12-20 15:02:35 +0100

    add player.py + player model

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,6 +21,8 @@ def main():
                 return
         screen.fill("black")
         
+        player1.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
