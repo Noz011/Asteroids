c46c397a1f2c1ba99a7aaee24a15913406e628a7
Ynoz <floriansonnleitner14@gmail.com>
2025-12-21 15:15:21 +0100

    add Asteroids and their movement

--- main.py
+++ main.py
@@ -2,16 +2,31 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Group definitions have to be called before creating Player object
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroids_field = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    #Creating Player object
     player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    asteroidField = AsteroidField()
+    #initialising Pygame
     pygame.init()
+    #Setting screen resolution
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #Creating Clock object
     clock = pygame.time.Clock()
     dt = 0
     while(True):
@@ -19,13 +34,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         
-        player1.update(dt)
-        player1.draw(screen)
+        #Updating the objects and drawing the screen
+        updatable.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+        
 
         pygame.display.flip()
-        
+        #setting Framerate
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from constants import PLAYER_RADIUS
 from constants import LINE_WIDTH
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 from circleshape import CircleShape
 import pygame
 
@@ -18,6 +19,12 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
@@ -28,6 +35,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
